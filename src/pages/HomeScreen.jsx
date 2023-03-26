2719835d2639a7e388d6da69b08d82dad87b68ee
mayurlalwani <lalwanimayur06@gmail.com>
2023-03-26 14:36:58 +0530

    implement search for products

--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,28 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import { listProducts } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Product from "../components/Product";
 import axios from "axios";
+import FormContainer from "../components/FormContainer";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+  const [productsList, setProductsList] = useState([]);
+  console.log({ productsList });
+  const handleSearch = (e) => {
+    const filterSearchProducts = products.filter((item) =>
+      item.title.toLowerCase().includes(e.target.value)
+    );
+    setProductsList(filterSearchProducts);
+  };
 
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
+  useEffect(() => {
+    setProductsList(products);
+  }, [products]);
+
   return (
     <>
-      <h1>Latest Products</h1>
+      <FormContainer>
+        <Form
+        //  onSubmit={submitHandler}
+        >
+          <Form.Group controlId="search">
+            <Form.Control
+              type="text"
+              placeholder="Search..."
+              // value={username}
+              onChange={handleSearch}
+            ></Form.Control>
+          </Form.Group>
+        </Form>
+      </FormContainer>
       {loading ? (
         <Loader />
       ) : (
         <Row>
-          {products.map((product) => (
+          {productsList.map((product) => (
             <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
